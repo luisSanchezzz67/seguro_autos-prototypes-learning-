128ee69f7bc1c0c1e0ec62140d851a386554865d
luis cruz <luiscrruzsanch3837@gmail.com>
2022-09-10 19:17:21 -0500

    Empezando la base del js

---
+++ js/app.js
@@ -0,0 +1,21 @@
+
+
+//Constructores
+
+function seguro(marca,year, tipo) {
+    this.marca = marca;
+    this.year = year;
+    this.tipo = tipo;
+}
+
+function UI(){
+
+}
+
+//Llena las opciones de los años
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+})
